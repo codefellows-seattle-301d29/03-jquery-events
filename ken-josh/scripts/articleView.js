6769f1521a40a04b4f41355a44258917210bace5
Joshua Fredrickson <uafredrickson@gmail.com>
2018-01-11 09:18:10 -0800

    added josh's notes

--- ken-josh/scripts/articleView.js
+++ ken-josh/scripts/articleView.js
@@ -13,10 +13,11 @@ articleView.populateFilters = function() {
       // Start by grabbing the author's name from `this` article element, and then use that bit of text to create the option tag (in a variable named `optionTag`) that we can append to the #author-filter select element.
       authorName = $(this).attr('data-author');
 
-      // TODO: Refactor this concatenation using a template literal.
-      optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+      // TOD: Refactor this concatenation using a template literal.
+      // optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+      optionTag = '<option value="` ${authorName} `">` ${authorName} `</option>';
 
-      if ($('#author-filter option[value="' + authorName + '"]').length === 0) {
+      if ($('#author-filter option[value="` ${authorName} ``"]').length === 0) {
         $('#author-filter').append(optionTag);
       }
 
@@ -25,7 +26,7 @@ articleView.populateFilters = function() {
       category = $(this).attr('data-category');
 
       // TODO: Refactor this concatenation using a template literal.
-      optionTag = '<option value="' + category + '">' + category + '</option>';
+      optionTag = '<option value="` ${category} ``">` ${category} `</option>';
 
       if ($('#category-filter option[value="' + category + '"]').length === 0) {
         $('#category-filter').append(optionTag);
@@ -39,7 +40,7 @@ articleView.handleAuthorFilter = function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
       // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
-      // Use an "attribute selector" to find those articles, and fade them in for the reader.
+      // Use an "attribute selector"   (!!!!!!! $('.active[id="turtle"])!!!!!!!) to find those articles, and fade (!!!!! use jQuery fade in!!!!!) them in for the reader.
 
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
@@ -53,7 +54,7 @@ articleView.handleCategoryFilter = function() {
   // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
-  // Be sure to reset the #author-filter while you are at it!
+  // Be sure to reset the #author-filter while you are at it!   (!!!!!checkout yesterdays demo!!!!!!)
 
 };
 
@@ -70,7 +71,7 @@ articleView.setTeasers = function() {
   // REVIEW: Hide elements beyond the first 2 in any article body.
   $('.article-body *:nth-of-type(n+2)').hide();
 
-  // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!   (!!!!!try to addd the read-less??!!!!!!!!)
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
 
