65d62de3a5e34556367f8b1330425ff898be8e8d
jayadams011 <j.adams011@hotmail.com>
2018-01-11 11:27:00 -0800

    " added ability to hide all articles and fade in selected.  added if selector blank show all but hide template."

--- jay-ahmed/scripts/articleView.js
+++ jay-ahmed/scripts/articleView.js
@@ -39,14 +39,14 @@ articleView.handleAuthorFilter = function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
       $('article').hide();
-      $('article[data-author===$(this).val()]').fadeIn();
+      $(`article[data-author="${$(this).val()}"]`).fadeIn();
       // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
 
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
-    $('article').show();
-    $('template').hide();
+      $('article').fadeIn();
+      $('article.template').hide();
     }
     $('#category-filter').val('');
   });
