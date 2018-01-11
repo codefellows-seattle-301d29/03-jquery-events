cad69b72c72acce06f5f8d56920a3ce4f847362c
AhmedAli93 <Ahmed98148@gmail.com>
2018-01-11 12:44:30 -0800

    finsihed up articleView

--- jay-ahmed/scripts/articleView.js
+++ jay-ahmed/scripts/articleView.js
@@ -39,14 +39,14 @@ articleView.handleAuthorFilter = function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
       $('article').hide();
-      $(`article[data-author="${$(this).val()}"]`).fadeIn();
+      $('article[data-author===$(this).val()]').fadeIn();
       // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
 
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
-      $('article').fadeIn();
-      $('article.template').hide();
+      $('article').show();
+      $('template').hide();
     }
     $('#category-filter').val('');
   });
@@ -57,6 +57,21 @@ articleView.handleCategoryFilter = function() {
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
+  $('#category-filter').on('change', function() {
+    // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category===$(this).val()]').fadeIn();
+      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      // Use an "attribute selector" to find those articles, and fade them in for the reader.
+
+    } else {
+      // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+      $('article').show();
+      $('template').hide();
+    }
+    $('#author-filter').val('');
+  });
 
 };
 
@@ -66,7 +81,12 @@ articleView.handleMainNav = function() {
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
-  $('.main-nav .tab:first').click();
+  $('.main-nav .tab').on('click', function(){
+    $('.tab-content').hide();
+    var tab = $(this).attr('data-content');
+    $(`#${tab}`).show();
+  });
+  $('main-nav .tab:first').click();
 };
 
 articleView.setTeasers = function() {
@@ -75,9 +95,17 @@ articleView.setTeasers = function() {
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+  $('articles').on('click', 'read-on', function(){
+    $(this).prev().children().show();
+    $(this).hide();
+  })
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
+  articleView.handleCategoryFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
 })
